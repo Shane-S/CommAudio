b1b417ecd9ccbb236adb553d8c3ad4429875496a
Abhishek Bhardwaj <abhishek66ster@gmail.com>
2014-04-07 00:17:42 -0700

    Refactored ConnectDialog::getDataAndClose()

--- src/client/commAudio/ConnectDialog.cpp
+++ src/client/commAudio/ConnectDialog.cpp
@@ -7,25 +7,10 @@ ConnectDialog::ConnectDialog(QWidget * parent) : QDialog(parent)
 
 void ConnectDialog::getDataAndClose()
 {
-    QString temp;
-    std::string temp1;
-    int temp2;
-
-    temp = ip_address->text();
-    temp1 = temp.toStdString();
-    connectionSettings.setIpAddress(temp1);
-
-    temp = tcp_port_number->text();
-    temp2 = temp.toInt();
-    connectionSettings.setPortTCP(temp2);
-
-    temp = udp_port_no->text();
-    temp2 = temp.toInt();
-    connectionSettings.setPortUDP(temp2);
-
-    temp = username->text();
-    temp1 = temp.toStdString();
-    connectionSettings.setUsername(temp1);
+    connectionSettings.setIpAddress(ip_address->text().toStdString());
+    connectionSettings.setPortTCP(tcp_port_number->text().toInt());
+    connectionSettings.setPortUDP(udp_port_no->text().toInt());
+    connectionSettings.setUsername(username->text().toStdString());
     
     close();
 }

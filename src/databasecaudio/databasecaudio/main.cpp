555cea7a6d9bbfadedbe6d85df50125512717162
razc411 <razc411@hotmail.ca>
2014-04-11 11:20:18 -0700

    okay revised code for saving

--- src/databasecaudio/databasecaudio/main.cpp
+++ src/databasecaudio/databasecaudio/main.cpp
@@ -40,27 +40,49 @@ int main(void)
 	return 0;
 }
 
-void saveFile(const char * buffer)
+int _kbhit()
+{
+	int r;
+	fd_set rfds;
+	struct timeval tv;
+	struct termios term, oterm;
+	tcgetattr(0, &oterm);
+	memcpy(&term, &oterm, sizeof(term));
+	cfmakeraw(&term);
+	tcsetattr(0, TCSANOW, &term);
+	FD_ZERO(&rfds);
+	FD_SET(0, &rfds);
+	tv.tv_sec = tv.tv_usec = 0;
+	r = select(1, &rfds, NULL, NULL, &tv);
+	tcsetattr(0, TCSANOW, &oterm);
+	return r;
+}
+
+void saveFile(const char * buffer, int bufsize, HSTREAM stream)
 {
 	FILE * fp;
 	BASS_CHANNELINFO info;
 	WAVEFORMATEX wf;
 
-	if (!(fp = fopen("BASS.WAV", "wb"))){
-		cout << "Failed to send to file." << endl;
+	if ((fp = fopen("stream.wav", "wb")) == 0)
+	{
+		cout << "Failed to open file" << endl;
+		return;
 	}
-	printf("writing to BASS.WAV file... press a key to stop\n");
-	BASS_ChannelGetInfo(2, &info);
+
+	//get and save the channel information
+	BASS_ChannelGetInfo(stream, &info);
+
 	wf.wFormatTag = 1;
 	wf.nChannels = info.chans;
 	wf.wBitsPerSample = (info.flags&BASS_SAMPLE_8BITS ? 8 : 16);
 	wf.nBlockAlign = wf.nChannels*wf.wBitsPerSample / 8;
 	wf.nSamplesPerSec = info.freq;
 	wf.nAvgBytesPerSec = wf.nSamplesPerSec*wf.nBlockAlign;
-
+	//write the header to the file first.
 	fwrite("RIFF\0\0\0\0WAVEfmt \20\0\0\0", 20, 1, fp);
 	fwrite(&wf, 16, 1, fp);
-	fwrite("data\0\0\0\0", 8, 1, fp);
 
-	fwrite(buffer, sizeof(buffer), 1, fp);
+	fwrite(buffer, sizeof(char), bufsize, fp);
+
 }
